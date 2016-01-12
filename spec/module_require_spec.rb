c1f7f51feaf906fafe79e2a44911507e5310491f
Stephen Best <bestie@gmail.com>
2016-01-12 16:48:36 +0000

    Rename to ModuleRequire

--- spec/module_require_spec.rb
+++ spec/module_require_spec.rb
@@ -1,30 +1,30 @@
 require "spec_helper"
 
-require "better_require"
+require "module_require"
 
 Module.class_eval do
-  include BetterRequire
+  include ModuleRequire
 end
 
 describe Module do
   let(:new_module)         { Module.new }
   let(:test_lib_file_path) { "spec/support/library" }
 
-  describe "#better_require" do
+  describe "#module_require" do
     it "returns nil" do
       expect(
-        new_module.better_require(test_lib_file_path)
+        new_module.module_require(test_lib_file_path)
       ).to be(nil)
     end
 
     it "loads the library module into the new module" do
-      new_module.better_require(test_lib_file_path)
+      new_module.module_require(test_lib_file_path)
 
       expect(new_module.constants).to eq([:Library])
     end
 
     it "does not bork the libraries internal constants" do
-      new_module.better_require(test_lib_file_path)
+      new_module.module_require(test_lib_file_path)
 
       library_in_new_module = new_module.const_get(:Library)
 
@@ -41,8 +41,8 @@ describe Module do
       let(:mod2) { Module.new }
 
       it "can load them into separate modules" do
-        mod1.better_require(test_lib_file_path)
-        mod2.better_require(test_lib_file_path)
+        mod1.module_require(test_lib_file_path)
+        mod2.module_require(test_lib_file_path)
 
         expect(mod1.constants).to include(:Library)
         expect(mod2.constants).to include(:Library)
@@ -53,13 +53,13 @@ describe Module do
       let(:test_lib_file_path) { "spec/support/library_with_support" }
 
       it "loads both library and supporting library module into the new module" do
-        new_module.better_require(test_lib_file_path)
+        new_module.module_require(test_lib_file_path)
 
         expect(new_module.constants).to match_array([:Library, :SupportingLibrary])
       end
 
       it "does not bork constant lookup within the library" do
-        new_module.better_require(test_lib_file_path)
+        new_module.module_require(test_lib_file_path)
 
         supporting_library_constants = new_module
           .const_get("SupportingLibrary::SupportingClass")
@@ -78,13 +78,13 @@ describe Module do
       let(:undesired_constants) { [:LibWithTwoTopLevelModulesBadModule] }
 
       it "imports the specified constants" do
-        new_module.better_require(test_lib_file_path, desired_constants)
+        new_module.module_require(test_lib_file_path, desired_constants)
 
         expect(new_module.constants).to include(*desired_constants)
       end
 
       it "rejects any further constants" do
-        new_module.better_require(test_lib_file_path, desired_constants)
+        new_module.module_require(test_lib_file_path, desired_constants)
 
         expect(new_module.constants).not_to include(*undesired_constants)
       end
