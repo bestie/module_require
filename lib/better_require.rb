ddc91d3ec8df6b2f9bbdcf60967873b6658162a3
Stephen Best <bestie@gmail.com>
2016-01-12 16:41:09 +0000

    Split out into own file

---
+++ lib/better_require.rb
@@ -0,0 +1,24 @@
+module ModuleRequire
+  def module_require(filepath, desired_constants = false)
+    filepath = filepath.slice(-3..-1) == ".rb" ? filepath : filepath + ".rb"
+    file_contents = File.read(filepath)
+
+    intermediate_module = Module.new
+    intermediate_module.module_eval(file_contents)
+    transferrable_constants = desired_constants ? desired_constants : intermediate_module.constants
+
+    copy_constants(intermediate_module, transferrable_constants)
+
+    nil
+  end
+
+  def require(filepath)
+    module_require(filepath)
+  end
+
+  def copy_constants(source, list)
+    list.each do |name|
+      const_set(name, source.const_get(name))
+    end
+  end
+end
--- spec/better_require_spec.rb
+++ spec/better_require_spec.rb
@@ -1,28 +1,9 @@
 require "spec_helper"
 
+require "better_require"
+
 Module.class_eval do
-  def better_require(filepath, desired_constants = false)
-    filepath = filepath.slice(-3..-1) == ".rb" ? filepath : filepath + ".rb"
-    file_contents = File.read(filepath)
-
-    intermediate_module = Module.new
-    intermediate_module.module_eval(file_contents)
-    transferrable_constants = desired_constants ? desired_constants : intermediate_module.constants
-
-    copy_constants(intermediate_module, transferrable_constants)
-
-    nil
-  end
-
-  def require(filepath)
-    better_require(filepath)
-  end
-
-  def copy_constants(source, list)
-    list.each do |name|
-      const_set(name, source.const_get(name))
-    end
-  end
+  include BetterRequire
 end
 
 describe Module do
